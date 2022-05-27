64bb1b94ec5e12aad6a94e4994f9604f733210bb
Marcin Jerzak <jerzakm@gmail.com>
2022-05-27 20:33:04 +0200

    - button hover and click animations

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -12,11 +12,25 @@
 	</button>
 {/if}
 
-<style>
+<style lang="scss">
 	.button {
-		border: 2px solid var(--border-color);
+		border: 4px inset var(--border-color);
 		box-shadow: var(--primary-color) 3px 3px;
+
 		@apply px-4 py-2 flex justify-between;
 		background-color: var(--background-color);
+		transform-origin: 50% 50%;
+		transition: 100ms all ease-in;
+		&:hover {
+			background-color: var(--text-color);
+			color: var(--background-color);
+			box-shadow: var(--primary-color) 5px 5px;
+			transform: scale(1.07);
+		}
+
+		&:active {
+			transform: scale(1.03);
+			box-shadow: var(--primary-color) 2px 2px;
+		}
 	}
 </style>
