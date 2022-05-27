6054726af9fe3cf2449327f9542c6807a65e34a1
Marcin Jerzak <jerzakm@gmail.com>
2022-05-27 14:17:36 +0200

    landing mockup, unresponsive

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,28 @@
+<header>
+	<div class="p-4 mx-auto text-2xl ">
+		<div class="flex items-center justify-between space-x-4 lg:space-x-10">
+			<div class="flex lg:w-0 lg:flex-1">
+				<a href="/" class="h-12 flex"><img src="tripsie.svg" alt="Tripsie website logo" /></a>
+			</div>
+
+			<nav class="hidden space-x-8 font-bold md:flex">
+				<a href="/">Places</a>
+				<a href="/">Creators</a>
+				<a href="/">Deals</a>
+				<a href="/">Community</a>
+				<a href="/">About</a>
+			</nav>
+
+			<div class="items-center justify-end flex-1 hidden space-x-4 sm:flex font-bold">
+				<a class="px-5 py-2 rounded-sm new-video" href="">Sign in </a>
+			</div>
+		</div>
+	</div>
+</header>
+
+<style>
+	.new-video {
+		border: 2px solid black;
+		box-shadow: #e55151 3px 3px;
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,91 @@
 <script lang="ts">
-	import Map from '$lib/components/Map.svelte';
 </script>
 
-<h1 class="text-4xl text-red-800">Test</h1>
+<hero>
+	<cta>
+		<div>
+			<h1>Explore the unknown.</h1>
+			<h2>with the world's first map based video browser</h2>
+		</div>
+	</cta>
 
-<Map />
+	<div class="graphics">
+		<img src="images/resting_person.svg" id="resting-person" />
+		<div id="hero-vid">
+			<img src="images/world_map_marked.svg" />
+			<div id="vid-wrapper">
+				<video autoplay loop muted>
+					<source src="videos/sanfran.mp4" type="video/mp4" />
+					Your browser does not support the video tag.
+					<track kind="captions" />
+				</video>
+			</div>
+
+			<button id="cta-start-btn">
+				<span>Start now</span>
+				<span>&gt</span>
+			</button>
+		</div>
+	</div>
+</hero>
+
+<style lang="scss">
+	#cta-start-btn {
+		@apply px-4 py-2 flex justify-between absolute;
+		font-size: 1.8rem;
+		border: 2px solid black;
+		box-shadow: #e55151 3px 3px;
+		background-color: #fcf4e7;
+		bottom: -120px;
+		right: 0;
+		width: 50%;
+
+		@apply z-20;
+	}
+
+	hero {
+		@apply flex mt-24 px-24 flex-col items-center;
+		min-height: 70vh;
+
+		cta {
+			@apply flex flex-col items-center;
+			div {
+				min-height: 20vh;
+			}
+		}
+		.graphics {
+			@apply flex;
+			position: relative;
+			#resting-person {
+				position: absolute;
+				bottom: 0;
+				transform: translate(-45%, 45%);
+				@apply z-20;
+			}
+
+			#hero-vid {
+				@apply flex flex-col;
+				border: 3px solid black;
+				border-radius: 4px;
+				width: 600px;
+				height: 320px;
+				position: relative;
+
+				#vid-wrapper {
+					overflow: hidden;
+					max-height: 100%;
+					position: relative;
+				}
+
+				img {
+					position: absolute;
+					height: 25%;
+					bottom: -2px;
+					right: -2px;
+					@apply z-20;
+				}
+				box-shadow: #000000 3px -3px;
+			}
+		}
+	}
+</style>
