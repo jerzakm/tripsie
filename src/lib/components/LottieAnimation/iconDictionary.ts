9c871ac1fa922f2103c558bf6990dddbcccd299a
Marcin Jerzak <jerzakm@gmail.com>
2022-05-28 23:05:19 +0200

    lottie animation component

---
+++ src/lib/components/LottieAnimation/iconDictionary.ts
@@ -0,0 +1,3 @@
+export const iconDictionary = {
+	confetti: '/icons/confetti.json'
+};
---
+++ src/lib/components/LottieAnimation/index.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+	import lottie from 'lottie-web';
+	import type { AnimationItem } from 'lottie-web';
+	import { onMount } from 'svelte';
+	import { iconDictionary } from './iconDictionary';
+
+	let container: HTMLElement;
+
+	export let loop = false;
+	export let autoplay = false;
+	export let icon: keyof typeof iconDictionary;
+	export let animation: AnimationItem | undefined = undefined;
+
+	onMount(() => {
+		animation = lottie.loadAnimation({
+			container, // the dom element that will contain the animation
+			renderer: 'svg',
+			loop,
+			autoplay,
+			path: iconDictionary[icon]
+		});
+	});
+</script>
+
+<animatedIcon bind:this={container} />
