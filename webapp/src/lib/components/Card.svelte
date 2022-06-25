9c6ed35054c100a343661bd2bf2791c8d407c441
Marcin Jerzak <hey@jerzakm.com>
2022-06-25 21:33:53 +0200

    Card component: - now accepts class props. - removed margin and max width

--- webapp/src/lib/components/Card.svelte
+++ webapp/src/lib/components/Card.svelte
@@ -1,13 +1,13 @@
-<card>
+<card class={$$props.class}>
 	<slot />
 </card>
 
 <style lang="scss">
 	card {
-		@apply mx-2 text-center px-4 py-8 xl:px-16 lg:pt-16 grow;
-		max-width: calc(20rem + 25vmin);
+		@apply text-center px-4 py-8 xl:px-16 lg:pt-16 grow;
+		// max-width: calc(20rem + 25vmin);
 		border: solid var(--border-color) 8px;
-		background-color: var(--background-color);
+		background-color: var(--surface-background-color);
 		box-shadow: var(--primary-color) 5px 5px;
 	}
 </style>
