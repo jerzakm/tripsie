1185c769bc481b08916ea0c940807b669dcedc5f
jerzakm <jerzakm@gmail.com>
2022-07-29 10:53:16 +0200

    marker dragging & updates

--- webapp/src/lib/components/Maps/SuggestLocationMap.svelte
+++ webapp/src/lib/components/Maps/SuggestLocationMap.svelte
@@ -6,29 +6,45 @@
 
 	let mapEl: any;
 
-	export let lngLat: [number, number] = [-90, -90];
+	export let lngLat: [number, number] | undefined;
+
+	let map: mapboxgl.Map;
+
+	let marker: mapboxgl.Marker | undefined;
 
 	onMount(() => {
 		mapboxgl.accessToken =
 			'pk.eyJ1IjoiamVyemFrbSIsImEiOiJjangxaHF4MGcwN3ZqNGJubzl2Zzdva3N5In0.DRchXs3ESLUuoH9Kh_N-ow';
 
-		const map = new mapboxgl.Map({
-			container: mapEl, // container ID
-			style: 'mapbox://styles/mapbox/streets-v11', // style URL
-			center: [-74.5, 40], // starting position [lng, lat]
-			zoom: 9 // starting zoom
+		map = new mapboxgl.Map({
+			container: mapEl,
+			style: 'mapbox://styles/mapbox/streets-v11',
+			center: [-74.5, 40],
+			zoom: 0
 		});
-		map.on('style.load', () => {
-			map.setFog({}); // Set the default atmosphere style
-		});
-
-		const marker = new mapboxgl.Marker({ draggable: true }).setLngLat(lngLat).addTo(map);
 
 		map.on('click', (e) => {
 			lngLat = [e.lngLat.lng, e.lngLat.lat];
-			marker.setLngLat(lngLat);
 		});
 	});
+
+	$: lngLat && map && updateMarker();
+
+	function updateMarker() {
+		if (!lngLat) return;
+		if (!marker) {
+			marker = new mapboxgl.Marker({ draggable: true }).setLngLat(lngLat).addTo(map);
+			marker.on('dragend', (e) => {
+				lngLat = [marker.getLngLat().lng, marker.getLngLat().lat];
+			});
+		}
+		marker.setLngLat(lngLat);
+		map.flyTo({
+			zoom: 4,
+			center: lngLat,
+			essential: true // this animation is considered essential with respect to prefers-reduced-motion,
+		});
+	}
 </script>
 
 <map bind:this={mapEl} class="w-full h-full" />
